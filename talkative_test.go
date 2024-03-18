dd89c1af840f3942ec7ee557a20dadcba2e51665
Rifaudeen <rifaudeen.ahamad@mywhoosh.com>
2024-03-18 15:19:21 +0400

    updated docstring

--- talkative_test.go
+++ talkative_test.go
@@ -7,6 +7,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// TestClient tests the behavior of the New function in two scenarios:
+// 1. When the URL parameter is empty or contains only whitespace characters, it should return an error of type talkative.ErrUrl and a nil client.
+// 2. When a valid URL is provided, it should return a non-nil client and no error.
+//
+// Parameters:
+// - t: A testing.T object used for running the test and reporting any failures.
 func TestClient(t *testing.T) {
 	t.Run("client-validation-error", func(t *testing.T) {
 		client, err := talkative.New(" ")
