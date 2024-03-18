9241899bcc709e02c4e0dab52d3c68fdced040c0
Rifaudeen <rifaudeen.ahamad@mywhoosh.com>
2024-03-18 14:54:30 +0400

    moved test cases into its own package to avoid any collisions

--- talkative_test.go
+++ talkative_test.go
@@ -1,6 +1,7 @@
-package talkative
+package talkative_test
 
 import (
+	"talkative"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,21 +9,17 @@ import (
 
 func TestClient(t *testing.T) {
 	t.Run("client-validation-error", func(t *testing.T) {
-		client, err := New(" ")
+		client, err := talkative.New(" ")
 
 		assert.Error(t, err)
-		assert.ErrorIs(t, err, ErrUrl)
+		assert.ErrorIs(t, err, talkative.ErrUrl)
 		assert.Nil(t, client)
 	})
 
 	t.Run("client-success", func(t *testing.T) {
-		client, err := New("http://localhost:11434")
+		client, err := talkative.New("http://localhost:11434")
 
 		assert.NoError(t, err)
 		assert.NotNil(t, client)
-
-		// Assert client urls  and client are set
-		assert.Equal(t, client.urls["chat"], "http://localhost:11434/api/chat")
-		assert.NotNil(t, client.client)
 	})
 }
