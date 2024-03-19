e23a190bb6513efa79474af409fd91ae0ba8a834
Rifaudeen <rifaudeen.ahamad@mywhoosh.com>
2024-03-19 15:06:43 +0400

    created reusable streaming response method

---
+++ streaming.go
@@ -0,0 +1,37 @@
+package talkative
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+// Streaming the response from the server asynchronously.
+//
+// This function takes an io.ReadCloser object (`body`) representing the response body
+// and a callback function (`cb`) for handling individual responses and errors.
+// It iterates through the response, decoding each message and invoking the callback for processing.
+//
+// In case of errors during decoding or processing, the callback is invoked with the error
+// and processing stops. The function closes the response body before exiting.
+func StreamResponse[T any](body io.ReadCloser, cb func(*T, error)) {
+	defer body.Close()
+
+	for {
+		var response T
+
+		err := json.NewDecoder(body).Decode(&response)
+
+		if err == io.EOF {
+			return
+		}
+
+		if err != nil {
+			cb(nil, fmt.Errorf("%w: %v", ErrDecoding, err))
+
+			return
+		}
+
+		cb(&response, nil)
+	}
+}
