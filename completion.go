fa559c5db0489c14b0bba8ba922f6e2b02afec8f
Rifaudeen <rifajas@gmail.com>
2024-03-23 14:31:58 +0400

    created PlainCompletion method with callback and updated test cases

--- completion.go
+++ completion.go
@@ -54,6 +54,14 @@ type CompletionMetrics struct {
 // - error: An error that might have occurred during the completion process.
 type CompletionCallback func(*CompletionResponse, error)
 
+// PlainCompletionCallback defines a function type that is used as a callback for handling plain completion responses.
+// It takes string and an error as arguments.
+//
+// Parameters:
+// - string: Json encoded plain string received after a completion request.
+// - error: An error that might have occurred during the completion process.
+type PlainCompletionCallback func(string, error)
+
 // Completion initiates a completion request to the server and returns a channel that signals when the operation is done.
 //
 // This method takes a CompletionCallback function and a CompletionMessage as arguments. The callback function is invoked
@@ -118,6 +126,63 @@ func (c *Client) Completion(model string, cb CompletionCallback, msg *Completion
 
 	go func() {
 		StreamResponse(res.Body, cb)
+
+		chDone <- true
+	}()
+
+	return chDone, nil
+}
+
+// Completion initiates a plain completion request to the server and returns a channel that signals when the operation is done.
+//
+// This method is identical to Completion(), except that it invokes the callback with plain json string without further processing.
+func (c *Client) PlainCompletion(model string, cb PlainCompletionCallback, msg *CompletionMessage) (<-chan bool, error) {
+	if cb == nil {
+		return nil, ErrCallback
+	}
+
+	if msg == nil {
+		return nil, ErrMessage
+	}
+
+	if model == "" {
+		model = DEFAULT_MODEL
+	}
+
+	request := CompletionRequest{
+		Model:  model,
+		Prompt: msg.Prompt,
+		Images: msg.Images,
+	}
+	body := &bytes.Buffer{}
+
+	if err := json.NewEncoder(body).Encode(request); err != nil {
+		return nil, fmt.Errorf("%w:%v", ErrEncoding, err)
+	}
+
+	res, err := c.client.Post(c.urls["completion"], "application/json", body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		switch res.StatusCode {
+		case http.StatusBadRequest:
+			defer res.Body.Close()
+
+			body, _ := io.ReadAll(res.Body)
+
+			return nil, fmt.Errorf("%w\n%v", ErrBadRequest, body)
+		default:
+			return nil, fmt.Errorf("%w: please make sure ollama server is running and url is correct", ErrInvoke)
+		}
+	}
+
+	chDone := make(chan bool, 1)
+
+	go func() {
+		StreamPlainResponse(res.Body, cb)
 
 		chDone <- true
 	}()
--- completion_test.go
+++ completion_test.go
@@ -89,7 +89,6 @@ func TestCompletionValidation(t *testing.T) {
 		assert.Nil(t, done)
 		assert.ErrorIs(t, err, talkative.ErrBadRequest)
 	}
-
 }
 
 func TestCompletionResponse(t *testing.T) {
@@ -157,6 +156,166 @@ func TestCompletionResponse(t *testing.T) {
 			fmt.Println("Error: ", err)
 		} else {
 			sb.WriteString(cr.Response)
+		}
+	}, message)
+
+	assert.NotNil(t, done)
+	assert.Nil(t, err)
+
+	<-done
+
+	assert.Equal(t, "Hello, It is nice talking to you.", sb.String())
+}
+
+func TestPlainCompletionValidation(t *testing.T) {
+	message := &talkative.CompletionMessage{
+		Prompt: "Hi there!",
+	}
+	scenario := "not-found"
+	server := mockServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if scenario == "not-found" {
+			w.WriteHeader(404)
+
+			return
+		}
+
+		if scenario == "non-json" {
+			w.Write([]byte("ok"))
+
+			return
+		}
+
+		if scenario == "bad-request" {
+			w.WriteHeader(400)
+			w.Write([]byte(`{"error": "invalid request"}`))
+			return
+		}
+
+		// Add more scenarios
+	}))
+
+	defer server.Close()
+
+	client, err := talkative.New(server.URL)
+	{
+		assert.NoError(t, err)
+		assert.NotNil(t, client)
+	}
+
+	done, err := client.PlainCompletion("", nil, nil)
+	{
+		assert.Nil(t, done)
+		assert.ErrorIs(t, err, talkative.ErrCallback)
+	}
+
+	// Assert no message error
+	done, err = client.PlainCompletion(talkative.DEFAULT_MODEL, func(cr string, err error) {}, nil)
+	{
+		assert.Nil(t, done)
+		assert.ErrorIs(t, err, talkative.ErrMessage)
+	}
+
+	// Begin scenario based validation
+
+	scenario = "not-found"
+	{
+		done, err = client.PlainCompletion(talkative.DEFAULT_MODEL, func(cr string, err error) {}, message)
+
+		assert.Nil(t, done)
+		assert.ErrorIs(t, err, talkative.ErrInvoke)
+	}
+
+	scenario = "non-json"
+	{
+		done, err = client.PlainCompletion(talkative.DEFAULT_MODEL, func(cr string, err error) {
+			assert.ErrorIs(t, err, talkative.ErrDecoding)
+		}, message)
+
+		assert.Nil(t, err)
+		assert.NotNil(t, done)
+
+		<-done // wait for completion
+	}
+
+	scenario = "bad-request"
+	{
+		done, err = client.PlainCompletion("", func(cr string, err error) {}, message)
+
+		assert.Nil(t, done)
+		assert.ErrorIs(t, err, talkative.ErrBadRequest)
+	}
+}
+
+func TestPlainCompletionResponse(t *testing.T) {
+	server := mockServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		responses := []talkative.CompletionResponse{
+			{
+				Model:    talkative.DEFAULT_MODEL,
+				Response: "Hello",
+			},
+			{
+				Model:    talkative.DEFAULT_MODEL,
+				Response: ", ",
+			},
+			{
+				Model:    talkative.DEFAULT_MODEL,
+				Response: "It is nice talking to you.",
+			},
+		}
+
+		w.Header().Add("Content-Type", "application/x-ndjson")
+		w.Header().Add("Transfer-Encoding", "chunked")
+
+		flusher, ok := w.(http.Flusher)
+
+		if !ok {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Server doesn't support flushing")
+			return
+		}
+
+		writer := json.NewEncoder(w)
+
+		for _, response := range responses {
+			err := writer.Encode(response)
+
+			if err != nil {
+				fmt.Println("error encoding response")
+				return
+			}
+
+			w.Write([]byte("\n"))
+
+			time.Sleep(100 * time.Millisecond)
+
+			flusher.Flush()
+		}
+	}))
+
+	defer server.Close()
+
+	client, err := talkative.New(server.URL)
+
+	// make sure client is set and no error
+	assert.NotNil(t, client)
+	assert.NoError(t, err)
+
+	message := &talkative.CompletionMessage{
+		Prompt: "Hi there!",
+	}
+
+	sb := strings.Builder{}
+
+	done, err := client.PlainCompletion("", func(cr string, err error) {
+		if err != nil {
+			fmt.Println("Error: ", err)
+		} else {
+			var response *talkative.CompletionResponse
+			json.Unmarshal([]byte(cr), &response)
+
+			if response != nil {
+				sb.WriteString(response.Response)
+			}
 		}
 	}, message)
 
